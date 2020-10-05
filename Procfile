f175a5022bb8829f28f374e7294487432842bff3
LMF <67340984+lmf-git@users.noreply.github.com>
2020-10-05 18:55:40 +0100

    Add mandatory web

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: node src/index.js
+web: node src/api/start.js
---
+++ src/api/start.js
@@ -0,0 +1,14 @@
+// Convert all lines below this (and after) to ESM
+require = require("esm")(module);
+
+import express from 'express';
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
