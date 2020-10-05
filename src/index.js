8d528e35d83cfb29bcd64db6b1fd66daeb59f0d8
LMF <67340984+lmf-git@users.noreply.github.com>
2020-10-05 06:08:05 +0100

    Add web server

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ require = require("esm")(module);
 // Bootstrap the client with ESM.
 import bootstrap from './bot/core/bootstrap';
 bootstrap();
+
+import express from 'express';
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
