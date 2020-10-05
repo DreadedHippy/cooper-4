eaa1e06e92cd1bbed3186f6a2a4ff1f117e961e5
LMF <67340984+lmf-git@users.noreply.github.com>
2020-10-05 20:00:04 +0100

    Test deploy

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ require = require("esm")(module);
 // Bootstrap the client
 const bootstrap = require('./bot/core/bootstrap');
 
+
 // After bootstrap IMPORT/EXPORT (ESM) will function.
 bootstrap();
