a8f321857ccc81746ed7c42c74e310d3733dbbe4
LMF <67340984+lmf-git@users.noreply.github.com>
2020-10-17 16:02:27 +0100

    Fix items helper

--- src/community/features/items/itemsHelper.js
+++ src/community/features/items/itemsHelper.js
@@ -13,7 +13,7 @@ export default class ItemsHelper {
                 VALUES($1, $2, $3) 
                 ON CONFLICT (owner_id, item_code)
                 DO 
-                UPDATE items SET quantity = quantity + $3 WHERE owner_id = $1 AND item_code = $2`,
+                UPDATE "items" SET quantity = quantity + $3 WHERE owner_id = $1 AND item_code = $2`,
             values: [userID, item_code, quantity]
         };
         return await Database.query(query);
