b83eb723348cc72971bbc3785d2f1a4890402ffb
LMF <67340984+lmf-git@users.noreply.github.com>
2020-11-07 01:25:58 +0000

    Update sacrifice voting logic

--- src/community/features/events/sacrificeHelper.js
+++ src/community/features/events/sacrificeHelper.js
@@ -35,7 +35,7 @@ export default class SacrificeHelper {
             
             // Calculate the number of required votes for the redemption poll.
             const reqSacrificeVotes = VotingHelper.getNumRequired(guild, .025);
-            const reqKeepVotes = VotingHelper.getNumRequired(guild, .015);
+            const reqKeepVotes = VotingHelper.getNumRequired(guild, .02);
         
             // Get existing reactions on message.
             let sacrificeVotes = 0;
@@ -45,23 +45,39 @@ export default class SacrificeHelper {
                 if (reactionType.emoji.name === EMOJIS.VOTE_FOR) keepVotes = Math.max(0, reactionType.count - 1);
             });
 
-            // TODO: Implement if enough keep votes, don't proccess sacrifice.
+            // Process votes with feedback for currently unprotected user.
+            const rawKeepVotes = reqKeepVotes - keepVotes;
+            if (rawKeepVotes > 0) {
+                const remainingProtectVotes = Math.max(0, rawKeepVotes);
+                const remainingSacrificeVotes = Math.max(0, reqSacrificeVotes - sacrificeVotes);   
 
-            // TODO: Add type of vote
-            // TODO: When type of vote is identified, only update for that type of vote.
-            const votingStatusTitle = `<@${targetMember.id}>'s sacrifice was voted upon! Remaining Votes:`;
-            const votingStatusText = votingStatusTitle +
-                `\nTo Protect: ${EMOJIS.VOTE_FOR}: ${Math.max(0, reqKeepVotes - keepVotes)} | ` +
-                `\nTo Sacrifice: ${EMOJIS.VOTE_AGAINST}: ${Math.max(0, reqSacrificeVotes - sacrificeVotes)}`;
+                // Check if enough votes to sacrifice.
+                if (remainingSacrificeVotes === 0) {
+                    // Notify when user is voted out.
+                    await ChannelsHelper._postToFeed(`<@${targetMember.id}>'s was sacrificed!`);
 
-            await ChannelsHelper._postToFeed(votingStatusText);
+                    // TODO: Implement.
+                    await ChannelsHelper._postToFeed('User should be banned... (note)');
 
-            // TODO: Notify when user is protected/kept.
-            // TODO: Notify when user is voted out.
-                    
+                } else {
+                    // Provide feedback for user who is not currently protected or sacrificed.
+                    await ChannelsHelper._postToFeed(
+                        `<@${targetMember.id}>'s sacrifice was voted upon!` +
+                        `\n\n**Remaining Votes:**` +
+                        `\nTo Protect: ${EMOJIS.VOTE_FOR}: ${remainingProtectVotes}` +
+                        `\nTo Sacrifice: ${EMOJIS.VOTE_AGAINST}: ${remainingSacrificeVotes}`
+                    );
+                }
+
+                
+            // Intercept latest vote granted protection to user.
+            } else if (rawKeepVotes === 0 && reaction.emoji.name === EMOJIS.VOTE_FOR) {
+                // Provide feedback for protected user.
+                await ChannelsHelper._postToFeed(`<@${targetMember.id}>'s was protected from sacrifice by votes!`);
+            } 
+
         } catch(e) {
             console.error(e);
         }
-
     }
 }
