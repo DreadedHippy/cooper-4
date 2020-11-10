eafec632b28295f7aa8b5a08ffa772c6ddbaea56
LMF <67340984+lmf-git@users.noreply.github.com>
2020-11-10 19:18:53 +0000

    Another face!

--- src/community/events/message/messageAdded.js
+++ src/community/events/message/messageAdded.js
@@ -36,13 +36,14 @@ export default function messageAddedHandler(msg) {
     const target = msg.mentions.users.first();
     if (target) {
 
-        
+
         // If targetting Cooper.
         if (target.id === STATE.CLIENT.user.id) {
             if (msg.content.indexOf(';-;') > -1) msg.say(';-;');
             if (msg.content.indexOf('._.') > -1) msg.say('._.');
             if (msg.content.indexOf(':]') > -1) msg.say(':]');
             if (msg.content.indexOf(':}') > -1) msg.say(':}');
+            if (msg.content.indexOf(':3') >-1) msg.say(':3');
         }
 
         if (
