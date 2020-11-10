f235b9a866cda97d261fa9379695fe582afb8102
LMF <67340984+lmf-git@users.noreply.github.com>
2020-11-10 18:38:31 +0000

    Minor sal improvement

--- src/bot/commands/misc/sal.js
+++ src/bot/commands/misc/sal.js
@@ -21,25 +21,7 @@ export default class SalCommand extends CoopCommand {
         setTimeout(() => {
             super.run(msg);
 
-            setTimeout(async () => {
-                const repeatNum = chanceInstance.natural({ min: 1, max: 20 });
-                const first = `Inklingboiiiiii?!?!??!!?!?!?! 🌋🌋🌋`;
-                const second = 'Ruuuuuu' + 'u'.repeat(repeatNum) + '!!!';
-                const third = ('🦑' + '🇩🇪').repeat(repeatNum);
-
-                const inklingMsg = await msg.say(first);
-
-                setTimeout(async () => {
-                    await inklingMsg.edit(second);
-                    setTimeout(() => { inklingMsg.react('🦑'); }, 500);
-
-                    setTimeout(async () => {
-                        await inklingMsg.edit(third);
-                        setTimeout(() => { inklingMsg.reactions.removeAll(); }, 250);
-                        setTimeout(() => { inklingMsg.react('🇩🇪'); }, 500);
-                    }, 3000);
-                }, 3000);
-            }, 300);
+            msg.say('Saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaal... Ru.')
         }, 250);
     }
     
