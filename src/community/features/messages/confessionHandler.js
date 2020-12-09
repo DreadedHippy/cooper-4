feb09debf2e80cbab8ef99ca8440ebbac863d84e
LMF <67340984+lmf-git@users.noreply.github.com>
2020-12-09 22:22:35 +0000

    Add confessions and responses

--- src/community/features/messages/confessionHandler.js
+++ src/community/features/messages/confessionHandler.js
@@ -4,36 +4,26 @@ import UsersHelper from "../../../bot/core/entities/users/usersHelper";
 export default class ConfessionHandler {
 
     static async onMessage(msg) {
-
-        console.log(msg);
-
-        console.log(msg.content);
-        console.log(msg.author);
-
-        console.log(msg.channel.type !== 'dm');
-        console.log(msg.isCommand);
-        console.log(UsersHelper.isCooperMsg(msg));
-
         if (msg.channel.type !== "dm") return false;
         if (UsersHelper.isCooperMsg(msg)) return false;
-        if (msg.isCommand) return false;
-
-        console.log('processing msg', msg.content);
+        if (msg.command !== null) return false;
 
         const annotatedMsgText = `DM message from ${msg.author.username}: ${msg.content}`;
-        ChannelsHelper._postToChannelCode('COOPERTESTS', annotatedMsgText);
+        ChannelsHelper._postToChannelCode('LEADERS', annotatedMsgText);
 
         setTimeout(async () => {
             const replyableMsg = await ChannelsHelper._postToChannelCode('COOPERTESTS', annotatedMsgText);
-            const filter = (reaction, user) => true;
-            replyableMsg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                .then(collected => {
-                    console.log(collected.size);
-                    console.log(collected);
+            replyableMsg.channel.awaitMessages(
+                () => true,
+                { max: 1, time: 30000, errors: ['time'] }
+            )
+            .then(responses => {
+                responses.map(resp => {
+                    UsersHelper._dm(msg.author.id, resp.content);
+                });
+            })
+            .catch(console.error);
 
-                    // Send the responses to the user.
-                })
-                .catch(collected => { console.log(collected); });
         }, 1333);
 
     }
