79808efffb2cb0d75fcc3fcb007779f5590a85ba
LMF <LMFielding@Liams-MacBook-Pro.local>
2020-12-13 21:21:20 +0000

    Push cors security modification

--- src/web/index.js
+++ src/web/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import Redis from '../bot/core/setup/redis';
 import socket from 'socket.io';
+import cors from 'cors';
 
 const app = express();
+
+// Put a hole in our own security, lulz.
+app.use(cors(
+    {
+        "origin": "https://thecoop.group",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
+));
 
 Redis.connect();
 Redis.connection.on('ready', () => {
@@ -13,7 +24,17 @@ Redis.connection.on('ready', () => {
     });
     
     const server = app.listen(process.env.PORT);
-    const socketio = socket(server);
+    const socketio = socket(server, {
+        cors: {
+            origin: "https://thecoop.group",
+            methods: ["*"]
+        }
+    });
+
+    let lastRedisUpdate;
+    setInterval(() => {
+        // Use certain redis values as a queue to update websockets connected to the coop.
+    }, 60 * 2 * 1000);
 
     socketio.on('connection', (socket) => {
         console.log('a user connected');
