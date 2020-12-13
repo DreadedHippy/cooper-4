6a0457e2f05d12f3944b7a412df7ce4481de5e69
LMF <LMFielding@Liams-MacBook-Pro.local>
2020-12-13 21:07:00 +0000

    Push websocket to web api

--- src/web/index.js
+++ src/web/index.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import Redis from '../bot/core/setup/redis';
+import socket from 'socket.io';
 
 const app = express();
 
+Redis.connect();
+Redis.connection.on('ready', () => {
+    console.log('Redis ready');
 
+    app.get('/', (req, res) => {
+        res.send('Coop Web API');
+    });
+    
+    const server = app.listen(process.env.PORT);
+    const socketio = socket.listen(server);
 
+    socketio.on('connection', (socket) => {
+        console.log('a user connected');
 
-app.listen(process.env.PORT);
-console.log(process.env.PORT);
+        socket.emit('TEST', 'FUCK YOU ARMADO');
+    });
+
+    console.log(`Listening on ${process.env.PORT}`);
+});
