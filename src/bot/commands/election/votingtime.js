3b4b3865603787d62e2dcba94e980568a834fb85
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-01-12 19:36:58 +0000

    Add voting time command

---
+++ src/bot/commands/election/votingtime.js
@@ -0,0 +1,37 @@
+import CoopCommand from '../../core/entities/coopCommand';
+
+import ElectionHelper from '../../community/features/hierarchy/election/electionHelper';
+import MessagesHelper from '../../core/entities/messages/messagesHelper';
+
+
+export default class VotingTimeCommand extends CoopCommand {
+
+	constructor(client) {
+		super(client, {
+			name: 'votingtime',
+			group: 'election',
+			memberName: 'votingtime',
+			aliases: [],
+			description: 'Check voting time remaining',
+			details: ``,
+			examples: ['votingtime']
+		});
+	}
+
+	async run(msg) {
+		super.run(msg);
+
+		const isOn = await ElectionHelper.isElectionOn();
+
+		if (isOn) {
+			const votingSecs = await ElectionHelper.votingPeriodLeftSecs();
+			const readableRemaining = TimeHelper.humaniseSecs(votingSecs);
+			MessagesHelper.selfDestruct(msg, `Voting time remaining: ${readableRemaining}`);
+
+		} else {
+			MessagesHelper.selfDestruct(msg, 'Election is not running.');
+		}
+
+    }
+    
+};
