634dff103d5d48b62ff02428eb75fdee8fafc461
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-01-24 23:59:39 +0000

    Fix link preview please

--- src/bot/community/features/messages/linkPreviewFilter.js
+++ src/bot/community/features/messages/linkPreviewFilter.js
@@ -6,12 +6,13 @@ import UsersHelper from "../../../core/entities/users/usersHelper";
 export default class LinkPreviewFilter {
 
         static async onMessage(msg) {
-            if (msg.channel.type !== "dm") return false;
             if (UsersHelper.isCooperMsg(msg)) return false;
             if (msg.command !== null) return false;
 
             const urlRegexExp = '(?:(?:http|https)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?';
             const regex = new RegExp(urlRegexExp, 'i');
+
+            console.log('link preview hit', console.log(msg.content));
 
             // Check if message contains link.
             if (regex.test(msg.content)) {
